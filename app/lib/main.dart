2a9591a836dfc5ed324e78f8ddf2ab3edd277b65
aarekr <aare212@hotmail.com>
2025-03-11 08:55:34 +0200

    AppBar, bottomNavigationBar with 3 buttons

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(title: Center(child: Text("Bookshelves"))),
         body: Center(
           child: Text('Hello World!'),
+        ),
+        bottomNavigationBar: Container(
+          height: 50,
+          child: Center(
+            child: Row(children: [
+              OutlinedButton(
+                child: Text("To Read"),
+                onPressed: () {},
+              ),
+              ElevatedButton(
+                child: Text("Reading"),
+                onPressed: () {},
+              ),
+              OutlinedButton(
+                child: Text("Completed"),
+                onPressed: () {},
+              ),
+            ]),
+          ),
         ),
       ),
     );
